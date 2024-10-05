7ad452c0bda634467eb6e537b1f756b649930422
DeMiro5001 <68397534+DeMiro5001@users.noreply.github.com>
2024-10-05 21:09:56 +0100

    Update config.inc.php

--- conf/config.inc.php
+++ conf/config.inc.php
@@ -309,23 +309,23 @@ allowed_title_html = "b,i,u,sup,sub"
 
 ; Default method to send emails
 ; Available options: sendmail, smtp, log, phpmailer
-default = sendmail
+default = smtp
 
 ; Path to the sendmail, -bs argument is for using SMTP protocol
 sendmail_path = "/usr/sbin/sendmail -bs"
 
 ; Use SMTP for sending mail instead of mail()
-; smtp = On
+ smtp = On
 
 ; SMTP server settings
-; smtp_server = mail.example.com
+ smtp_server = localhost
 ; smtp_port = 25
 
 ; Enable SMTP authentication
 ; Supported smtp_auth: ssl, tls (see PHPMailer SMTPSecure)
 ; smtp_auth = ssl
-; smtp_username = username
-; smtp_password = password
+ smtp_username = __APP__
+ smtp_password = __MAIL_PWD__
 
 ; Enable suppressing SSL/TLS peer verification by SMTP transports
 ; Note: this is not recommended for security reasons
