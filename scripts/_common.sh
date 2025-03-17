85e4aa642e85e0e49a324de8992a79f67b867790
DeMiro5001 <68397534+DeMiro5001@users.noreply.github.com>
2025-03-17 22:54:48 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,17 +6,17 @@
 
 fetch_dynamic_ids()  {
 
-curl -s https://$domain$path/index.php/index/install/install > form.html
+curl -s https://$domain$path/index.php/index/install/install > /tmp/form.html
 
-adminUsername_id=$(grep -oP 'id="adminUsername-\K[^"]+' form.html)
-adminPassword_id=$(grep -oP 'id="adminPassword-\K[^"]+' form.html)
-adminPassword2_id=$(grep -oP 'id="adminPassword2-\K[^"]+' form.html)
-adminEmail_id=$(grep -oP 'id="adminEmail-\K[^"]+' form.html)
-filesDir_id=$(grep -oP 'id="filesDir-\K[^"]+' form.html)
-databaseHost_id=$(grep -oP 'id="databaseHost-\K[^"]+' form.html)
-databaseUsername_id=$(grep -oP 'id="databaseUsername-\K[^"]+' form.html)
-databasePassword_id=$(grep -oP 'id="databasePassword-\K[^"]+' form.html)
-databaseName_id=$(grep -oP 'id="databaseName-\K[^"]+' form.html)
-oaiRepositoryId_id=$(grep -oP 'id="oaiRepositoryId-\K[^"]+' form.html)
+adminUsername_id=$(grep -oP 'id="adminUsername-\K[^"]+' /tmp/form.html)
+adminPassword_id=$(grep -oP 'id="adminPassword-\K[^"]+' /tmp/form.html)
+adminPassword2_id=$(grep -oP 'id="adminPassword2-\K[^"]+' /tmp/form.html)
+adminEmail_id=$(grep -oP 'id="adminEmail-\K[^"]+' /tmp/form.html)
+filesDir_id=$(grep -oP 'id="filesDir-\K[^"]+' /tmp/form.html)
+databaseHost_id=$(grep -oP 'id="databaseHost-\K[^"]+' /tmp/form.html)
+databaseUsername_id=$(grep -oP 'id="databaseUsername-\K[^"]+' /tmp/form.html)
+databasePassword_id=$(grep -oP 'id="databasePassword-\K[^"]+' /tmp/form.html)
+databaseName_id=$(grep -oP 'id="databaseName-\K[^"]+' /tmp/form.html)
+oaiRepositoryId_id=$(grep -oP 'id="oaiRepositoryId-\K[^"]+' /tmp/form.html)
 
 }
