1b9109527b24cb6dd3a018e3acffbc0f3257e24b
DeMiro5001 <68397534+DeMiro5001@users.noreply.github.com>
2025-03-17 22:22:02 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,3 +3,20 @@
 #=================================================
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
+
+fetch_dynamic_ids()  {
+
+curl -s index.php/index/install/install > form.html
+
+adminUsername_id=$(grep -oP 'id="adminUsername-\K[^"]+' form.html)
+adminPassword_id=$(grep -oP 'id="adminPassword-\K[^"]+' form.html)
+adminPassword2_id=$(grep -oP 'id="adminPassword2-\K[^"]+' form.html)
+adminEmail_id=$(grep -oP 'id="adminEmail-\K[^"]+' form.html)
+filesDir_id=$(grep -oP 'id="filesDir-\K[^"]+' form.html)
+databaseHost_id=$(grep -oP 'id="databaseHost-\K[^"]+' form.html)
+databaseUsername_id=$(grep -oP 'id="databaseUsername-\K[^"]+' form.html)
+databasePassword_id=$(grep -oP 'id="databasePassword-\K[^"]+' form.html)
+databaseName_id=$(grep -oP 'id="databaseName-\K[^"]+' form.html)
+oaiRepositoryId_id=$(grep -oP 'id="oaiRepositoryId-\K[^"]+' form.html)
+
+}
