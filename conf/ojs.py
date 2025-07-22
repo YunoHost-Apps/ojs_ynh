b7a13f0305371741e4c8b0354e570729332094b7
DeMiro5001 <68397534+DeMiro5001@users.noreply.github.com>
2025-07-22 12:53:58 +0100

    Update ojs.py

--- conf/ojs.py
+++ conf/ojs.py
@@ -95,9 +95,9 @@ with sync_playwright() as p:
 
     print("Submitting the form...")
     try:
-        page.wait_for_selector('button[name="submitFormButton"]', state="visible", timeout=60000)
+        page.wait_for_selector('button[name="submitFormButton"]', state="visible", timeout=360000)
         with page.expect_navigation(timeout=60000):
-            page.click('button[name="submitFormButton"]' , timeout=60000)
+            page.click('button[name="submitFormButton"]' , timeout=360000)
         print("Form submitted successfully.")
     except Exception as e:
         print(f"Error submitting the form: {e}")
